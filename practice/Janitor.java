3327dfd4395827b64623252c366d0959abaf8b42
Dokrimare <aditya.dokrimare@dxc.com>
2022-01-13 14:39:13 -0700

    code is comited.

---
+++ practice/Janitor.java
@@ -0,0 +1,28 @@
+package practice;
+public class Janitor extends Employee {
+    public int getHours() {
+        return 2 * super.getHours();
+    }
+    
+    public double getSalary() {
+        return super.getSalary() - 10000;
+    }
+    
+    public int getVacationDays() {
+        return super.getVacationDays() / 2;
+    }
+    
+    public void clean() {
+        System.out.println("Workin' for the man.");
+    }
+    public static void main(String[] args) {
+
+        Janitor janitor= new Janitor();
+        int a=janitor.getHours();
+        double b=janitor.getSalary();
+        int c=janitor.getVacationDays();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
